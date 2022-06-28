f59d9c7dbbb9d1e43cd355a4cc282735fa1e3225
vladyuh <33131143+vladyuh@users.noreply.github.com>
2022-06-28 17:26:14 +0300

    appoint

--- src/components/AppointmentBlock.vue
+++ src/components/AppointmentBlock.vue
@@ -32,11 +32,18 @@
                         stroke-linejoin="round"/>
                 </svg>
               </div>
-              <div class="caption">Выбрать услугу</div>
+              <div class="caption" v-show="!serviceSelected.length">Выбрать услугу</div>
+              <div class="caption" v-show="serviceSelected.length">
+                <div class="caption-title">{{ serviceSelected }}</div>
+                <div class="caption-desc" v-if="serviceChosen.length===0">Выберите услугу</div>
+                <div class="caption-desc" v-if="serviceChosen.length!==0">{{ serviceChosen }}</div>
+              </div>
             </div>
             <div class="appointment-block__item appointment-block__item--doctor" v-on:click="activeStep=3">
-              <div class="icon">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <div class="icon"
+                   v-bind:style="doctorChosen.length ? { 'background-image': 'url(' + getDoctor(doctorChosen,'image') + ')' } : { 'background-image': 'none' }">
+                <svg v-show="!doctorChosen.length" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                     xmlns="http://www.w3.org/2000/svg">
                   <path
                       d="M17 21V19C17 17.9391 16.5786 16.9217 15.8284 16.1716C15.0783 15.4214 14.0609 15 13 15H5C3.93913 15 2.92172 15.4214 2.17157 16.1716C1.42143 16.9217 1 17.9391 1 19V21"
                       stroke="#0088CC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -51,7 +58,11 @@
                       stroke="#0088CC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
               </div>
-              <div class="caption">Выбрать врача</div>
+              <div class="caption" v-show="!doctorChosen.length">Выбрать врача</div>
+              <div class="caption" v-show="doctorChosen.length">
+                <div class="caption-title">{{ getDoctor(doctorChosen, 'name') }}</div>
+                <div class="caption-desc">{{ getDoctor(doctorChosen, 'job') }}</div>
+              </div>
             </div>
             <div class="appointment-block__item appointment-block__item--date" v-on:click="activeStep=4">
               <div class="icon">
@@ -71,10 +82,19 @@
       </div>
       <div class="appointment-block" v-show="activeStep===1">
         <div class="container">
-          <div class="appointment-block__directions">
+          <div class="appointment-block__directions" v-if="!doctorChosen.length">
             <div class="appointment-block__direction" v-for="(item, i) in services" :key="i">
-              <label>{{item.name}}
-                <input type="radio" v-bind:value="item.name" v-model="serviceSelected" v-on:click="activeStep=2" v-bind:data-index="item.id" name="direction"/>
+              <label>{{ item.name }}
+                <input type="radio" v-bind:value="item.name" v-model="serviceSelected" v-on:click="activeStep=2"
+                       name="direction"/>
+              </label>
+            </div>
+          </div>
+          <div class="appointment-block__directions" v-if="doctorChosen.length">
+            <div class="appointment-block__direction" v-for="(item, i) in filteredServices" :key="i">
+              <label>{{ item.name }}
+                <input type="radio" v-bind:value="item.name" v-model="serviceSelected" v-on:click="activeStep=2"
+                       name="direction"/>
               </label>
             </div>
           </div>
@@ -83,10 +103,62 @@
       <div class="appointment-block" v-show="activeStep===2">
         <div class="container">
           <div class="appointment-block__services">
-            <div class="appointment-block__service" v-for="(item, i) in getServiceId(serviceSelected)" :key="i">
-              <label>{{item}}
-                <input type="radio" name="service"/>
+            <div class="appointment-block__service" v-for="(item, i) in getServiceItems(serviceSelected)" :key="i">
+              <label>{{ item }}
+                <input type="radio" v-bind:value="item" v-model="serviceChosen" v-on:click="activeStep=0"
+                       name="service"/>
               </label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="appointment-block" v-show="activeStep===3">
+        <div class="container">
+          <div class="appointment-block__doctors doctors-block__items" v-if="serviceSelected.length">
+            <div class="doctors-block__item" v-for="(item, i) in filteredItems" :key="i">
+              <label class="text">
+                <div class="text-name">{{ item.name }}</div>
+                <div class="text-caption">{{ item.job }}</div>
+                <input type="radio" name="doctor" v-bind:value="item.name" v-model="doctorChosen"
+                       v-on:click="activeStep=0"/>
+              </label>
+              <div class="image"><img class="lazyload" loading="lazy" v-bind:src="item.image" width="80" height="104"/>
+              </div>
+            </div>
+          </div>
+          <div class="appointment-block__doctors doctors-block__items" v-if="!serviceSelected.length">
+            <div class="doctors-block__item" v-for="(item, i) in doctors" :key="i">
+              <label class="text">
+                <div class="text-name">{{ item.name }}</div>
+                <div class="text-caption">{{ item.job }}</div>
+                <input type="radio" name="doctor" v-bind:value="item.name" v-model="doctorChosen"
+                       v-on:click="activeStep=0"/>
+              </label>
+              <div class="image"><img class="lazyload" loading="lazy" v-bind:src="item.image" width="80" height="104"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="appointment-block" v-show="activeStep===4">
+        <div class="container">
+          <div class="appointment-block__date">
+            <div class="appointment-block__datepicker">
+              <FunctionalCalendar v-model="calendarData" :configs="calendarConfigs" v-on:dayClicked="dayClicked($event)"></FunctionalCalendar>
+            </div>
+            <div class="appointment-block__time">
+              <label class="item">07:45
+                <input type="radio" name="time" value="07:45"/>
+              </label>
+              <label class="item">18:50
+                <input type="radio" name="time" value="18:50"/>
+              </label>
+              <label class="item">19:10
+                <input type="radio" name="time" value="19:10"/>
+              </label>
+            </div>
+            <div class="appointment-block__choose">
+              <div class="btn btn-cyan">Выбрать время приема</div>
             </div>
           </div>
         </div>
@@ -96,9 +168,13 @@
 </template>
 
 <script>
+import {FunctionalCalendar} from 'vue-functional-calendar';
 
 export default {
   name: 'AppointmentBlock',
+  components: {
+    FunctionalCalendar
+  },
   data: function () {
     return {
       activeStep: 0,
@@ -117,35 +193,118 @@ export default {
           id: 2,
           title: "Услуга",
           backIndex: 1,
+        },
+        {
+          id: 3,
+          title: "Врач",
+          backIndex: 0,
+        },
+        {
+          id: 4,
+          title: "Дата и время приема",
+          backIndex: 0,
         }
       ],
       services: [
         {
           id: 0,
-          name: "Гастроэнтерология 1",
+          name: "Гастроэнтерология",
           items: ["Гастроэнтерология 1 1", "Гастроэнтерология 1 2"],
-          doctors: [
+        },
+        {
+          id: 1,
+          name: "Гинекология",
+          items: [
+            "Кардиотокограмма плода",
+            "Первичный прием врача-акушера-гинеколога (детский)",
+            "Первичный прием врача-акушера-гинеколога с осмотром на гинекологическом кресле",
+            "Первичный прием врача-акушера-гинеколога без осмотра на гинекологическом кресле",
+            "Повторный прием врача-акушера-гинеколога с осмотром на гинекологическом кресле",
+            "Повторный прием врача-акушера-гинеколога без осмотра на гинекологическом кресле"
+          ],
+        }
+      ],
+      doctors: [
+        {
+          id: 0,
+          serviceId: 0,
+          name: "Подольская Елена Олеговна",
+          job: "Врач-гастроэнтеролог",
+          image: "/img/common/dc-2.jpg",
+          dates: [
             {
-              id: 0,
-              name: "Басенко Татьяна Валерьевна",
-              job: "Врач акушер-гинеколог"
+              date: '29/6/2022',
+              time: ['08:15', '09:15']
+            },
+            {
+              date: '30/6/2022',
+              time: ['10:15', '12:15']
             }
           ]
         },
         {
           id: 1,
-          name: "Гастроэнтерология 2",
-          items: ["Гастроэнтерология 2 1", "Гастроэнтерология 2 1"],
-          doctors: [
+          serviceId: 1,
+          name: "Басенко Татьяна Валерьевна",
+          job: "Врач акушер-гинеколог",
+          image: "/img/common/dc-8.jpg",
+          dates: [
             {
-              id: 0,
-              name: "Басенко Татьяна Валерьевна",
-              job: "Врач акушер-гинеколог",
+              date: '29/6/2022',
+              time: ['08:15', '09:15']
+            },
+            {
+              date: '30/6/2022',
+              time: ['10:15', '12:15']
+            }
+          ]
+        },
+        {
+          id: 2,
+          serviceId: 1,
+          name: "Ломаченко Мария Викторовна",
+          job: "Врач акушер-гинеколог",
+          image: "/img/common/doc-9.jpg",
+          dates: [
+            {
+              date: '29/6/2022',
+              time: ['08:15', '09:15']
+            },
+            {
+              date: '30/6/2022',
+              time: ['10:15', '12:15']
+            }
+          ]
+        },
+        {
+          id: 3,
+          serviceId: 1,
+          name: "Ивашкевич Надежда Владимировна",
+          job: "Врач акушер-гинеколог",
+          image: "/img/common/doc-10.jpg",
+          dates: [
+            {
+              date: '29/6/2022',
+              time: ['08:15', '09:15']
+            },
+            {
+              date: '30/6/2022',
+              time: ['10:15', '12:15']
             }
           ]
         }
       ],
       serviceSelected: [],
+      serviceChosen: [],
+      doctorChosen: [],
+      calendarData: {},
+      calendarConfigs: {
+        sundayStart: false,
+        dateFormat: 'dd/mm/yyyy',
+        isDatePicker: false,
+        isDateRange: false,
+        enabledDates: [],
+      },
     }
   },
   methods: {
@@ -155,19 +314,58 @@ export default {
     moveBack: function (index) {
       return this.steps[index].backIndex
     },
-    getServiceId: function (id){
-      if(this.serviceSelected.length){
-        let result = this.services.find(function (item){
+    getServiceById: function (name) {
+      if (this.serviceSelected.length) {
+        let result = this.services.find(function (item) {
+          return item.name === name
+        })
+        return result.id
+      }
+    },
+    getServiceItems: function (id) {
+      if (this.serviceSelected.length) {
+        let result = this.services.find(function (item) {
           return item.name === id
         })
         return result.items
       }
+    },
+    getDoctor: function (id, key) {
+      if (this.doctorChosen.length) {
+        let result = this.doctors.find(function (item) {
+          return item.name === id
+        })
+        return result[key]
+      }
+    },
+    dayClicked: function (event){
+      console.log(event)
     }
   },
   watch: {
-    serviceSelected: function (){
-      console.log(this.serviceSelected);
+    serviceSelected: function () {
+      this.serviceChosen = []
+    },
+    doctorChosen: function (){
+      let doc = this.getDoctor(this.doctorChosen,'dates');
+      doc.forEach( (el) => {
+        this.calendarConfigs.enabledDates.push(el.date)
+      })
     }
+  },
+  computed: {
+    filteredItems() {
+      return this.doctors.filter((item) => {
+        return item.serviceId === this.getServiceById(this.serviceSelected)
+      });
+    },
+    filteredServices() {
+      return this.services.filter((item) => {
+        return item.id === this.getDoctor(this.doctorChosen, 'serviceId')
+      });
+    },
+  },
+  mounted() {
   }
 }
 </script>
